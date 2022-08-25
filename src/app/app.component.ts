4a472d6ccb9109ce252b824b8408fc8c9e193954
Sai Teja Kommuri <32244803+saiteja77@users.noreply.github.com>
2022-08-24 20:37:28 -0500

    adde menu items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './interfaces/menu-item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'menu';
+  title = 'Welcome to Chirpus Pizza';
+
+  menuItems: MenuItem[] = [{
+      item: 'Chicken Fingers',
+      category: 'dinner',
+      price: 11.99
+    },
+    {
+      item: 'Pizza',
+      category: 'dinner',
+      price: 11.99
+    },
+    {
+      item: 'Wings',
+      category: 'sides',
+      price: 8.99
+    },
+    {
+      item: 'Breadsticks',
+      category: 'sides',
+      price: 4.99
+    },
+    {
+      item: 'Caesar Salad',
+      category: 'salads',
+      price: 5.99
+    },
+    {
+      item: 'Cinnamon Roll',
+      category: 'dessert',
+      price: 8.99
+    }
+  ];
+
+
 }
